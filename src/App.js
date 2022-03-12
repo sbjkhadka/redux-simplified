fdb632509a2888bd1b6684258309ea1345880e98
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-03-12 02:36:03 -0500

    Implemented in app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import ReduxDemo from "./ReduxDemo";
 import { actionCreators } from "./state/action-creators/exporter";
 import store from "./state/store";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   store.subscribe(() => console.log("Store is now: ", store.getState()));
+  const myState = useSelector((state) => state);
   // store.dispatch({ type: "deposit", payload: 1000 });
   // store.dispatch({ type: "withdraw", payload: 500 });
   // store.dispatch({ type: "name" });
@@ -15,8 +16,12 @@ function App() {
     bindActionCreators(actionCreators, useDispatch());
   return (
     <div className="App">
+      <h2>{myState.account}</h2>
+      <h2>{myState.inquiry}</h2>
       <button onClick={() => depositMoney(1000)}>Deposit</button>
       <button onClick={() => withDrawMoney(1000)}>Withdraw</button>
+      <button onClick={() => getBankName()}>Bank Name</button>
+      <button onClick={() => getAccountType()}>Account Type</button>
     </div>
   );
 }
