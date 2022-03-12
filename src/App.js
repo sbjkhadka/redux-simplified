df03b8951770f3198b3cb9a625700dcf2fd5deee
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-03-12 02:36:37 -0500

    Refactored app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import { bindActionCreators } from "redux";
 import "./App.css";
-import ReduxDemo from "./ReduxDemo";
 import { actionCreators } from "./state/action-creators/exporter";
 import store from "./state/store";
 import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  store.subscribe(() => console.log("Store is now: ", store.getState()));
-  const myState = useSelector((state) => state);
-  // store.dispatch({ type: "deposit", payload: 1000 });
-  // store.dispatch({ type: "withdraw", payload: 500 });
-  // store.dispatch({ type: "name" });
-  // store.dispatch({ type: "accountType" });
   const { depositMoney, withDrawMoney, getBankName, getAccountType } =
     bindActionCreators(actionCreators, useDispatch());
   return (
